00cc7aaa6e2e176182e34a90f358c318fa8d3f9d
Sierra W <WarElephant27@users.noreply.github.com>
2019-07-19 13:04:18 -0400

    2019 Updates
    Updating gulpfile etc for 2019

--- gulpfile.js
+++ gulpfile.js
@@ -1,102 +1,28 @@
-/*!
- * gulp
- * $ npm install gulp-ruby-sass gulp-autoprefixer gulp-cssnano 
- gulp-jshint gulp-concat gulp-uglify gulp-imagemin gulp-notify gulp-rename 
- gulp-livereload gulp-cache del gulp-nunjucks gulp-connect gulp--save-dev
- */
+// compile sass https://github.com/dlmanning/gulp-sass
+// render nunjucks partials https://www.npmjs.com/package/gulp-nunjucks-html
+// watch for js changes
+// watch
+// live refresh https://www.npmjs.com/package/gulp-refresh
+// jshint https://www.npmjs.com/package/gulp-jshint
+// optimize images https://www.npmjs.com/package/gulp-smushit
+//
+// tutorials http://zetcode.com/gulp/sass/
 
-var gulp = require('gulp'),
-  connect = require('gulp-connect');
-  nunjucks = require('gulp-nunjucks-render');
-  sass = require('gulp-sass');
+const 
+	sass = require('gulp-sass'),
+	nunjucks = require('gulp-nunjucks-html');
 
+function sass() {
+	// compile sass with gulp-scss
+}
 
-// Scripts
-gulp.task('scripts', function() {
-  return gulp.src('./app/scripts/**/*.js')
-    // .pipe(jshint('.jshintrc'))
-    .pipe(jshint.reporter('default'))
-    .pipe(concat('main.js'))
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(uglify())
-    .pipe(gulp.dest('dist/scripts'))
-    .pipe(notify({ message: 'Scripts task complete' }));
-});
+function nunjucks() {
+	// render nunjucks with gulp-nunjucks-html
+}
 
-/* connect server */
-gulp.task('connect', function() {
-  connect.server({
-    root: 'dist',
-    livereload: true
-  });
-});
+function defaultTask(gulp) {
+  // place code for your default task here
+  gulp();
+}
 
-/* Compile Nunjucks partials */
-gulp.task('nunjucks', function() {
-  // Gets .html and .nunjucks files in pages
-  return gulp.src('app/pages/**/*.+(html|nunjucks)')
-  // Renders template with nunjucks
-  .pipe(nunjucks({
-      path: ['app/templates']
-    }))
-  // output files in app folder
-  .pipe(gulp.dest('dist'))
-});
-
-/* compile sass */
-gulp.task('sass', function () {
-  return gulp.src('./app/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./dist'));
-});
-
-/* reload HTML */
-gulp.task('html', function () {
-  gulp.src('./dist/*.html')
-    .pipe(connect.reload());
-});
-
-/* reload CSS */
-gulp.task('css', function () {
-  gulp.src('./dist/styles/*.css')
-    .pipe(connect.reload());
-});
- 
-gulp.task('watch', function () {
-  //watch sass files
-  gulp.watch('./app/**/*.scss', gulp.parallel('sass'));
-
-  //watch nunjucks files 
-  gulp.watch('./app/**/*.nunjucks', gulp.parallel('nunjucks'));
-
-  // // Watch HTML in dist dir
-  // gulp.watch(['./dist/*.html'], ['html']);
-  gulp.watch('./dist/*.html', gulp.parallel('html'));
-
-
-  // // Watch CSS 
-  // gulp.watch(['./dist/styles/*.css'], ['css']);
-  gulp.watch('./dist/styles/*.css', gulp.parallel('css'));
-
-  // // Watch .js files
-  // gulp.watch('./dist/scripts/**/*.js', ['scripts']);
-  gulp.watch('./dist/scripts/**/*.js', gulp.parallel('scripts'));
-
-});
-
-/* stick connect and watch tasks together */
-gulp.task('build', gulp.series(
-  gulp.parallel('connect', 'watch')
-));
-
-/* connect server and watch */
-gulp.task('default', gulp.series('build'));
-
-
-
-
-
-
-
-
+exports.default = defaultTask
